72df21c7089178a144a316a49ea6688270a5dadd
Pu, Yiyu <yiyu.pu@intel.com>
2023-01-28 17:27:52 +0800

    test

--- csapp_lap/cache-lab/cachelab-handout/csim.c
+++ csapp_lap/cache-lab/cachelab-handout/csim.c
@@ -1,7 +1,17 @@
 #include "cachelab.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+/* parser */
+
+/* LRU */
+
+/* formal execution */
 
 int main()
 {
+
     printSummary(0, 0, 0);
     return 0;
 }
